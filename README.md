ca4a1261accf1c9bc0723739176b0b4ddc971276
Miros-iStore <>
2022-07-05 09:21:24 +0300

    new royal

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# AngularSandbox
+# RoyalCrm
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.4.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,10 @@
 import { Component } from '@angular/core';
-import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-sandbox';
-  test = Swal.fire({
-    title: 'Error!',
-    text: 'Do you want to continue',
-    icon: 'error',
-    confirmButtonText: 'Cool'
-  })
+  title = 'royal-crm';
 }
-
-
